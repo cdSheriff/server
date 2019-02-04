0abe3cfb8fbb49bbf005f981effc64ea9a6d0eb5
Colin Dess <pcolind@gmail.com>
2019-02-04 12:12:12 -0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,10 @@ module.exports = {
 	deploy: {
 		production: {
 			user: 'ubuntu',
-			host: 'ec2-13-58-157-199.us-east-2.compute.amazonaws.com'
+			host: 'ec2-13-58-157-199.us-east-2.compute.amazonaws.com',
 			key: '~/.ssh/test.pem',
 			ref: 'origin/master',
-			repo: 'git@github.com/cdSheriff/server.git'
+			repo: 'https://github.com/cdSheriff/server.git',
 			path: '/home/ubuntu/server',
 			'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
 		}
