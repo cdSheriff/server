7c55e9206b8a06bc713d48d37fcf6dbc22f07a4c
Colin Dess <pcolind@gmail.com>
2019-02-04 22:04:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-	res.send('look I can change!')
-})
+// app.get('/', (req, res) => {
+// 	res.send('look I can change!')
+// })
 
+app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Server running on port 3000'))
