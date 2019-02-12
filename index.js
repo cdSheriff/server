b639aa774727f37b49304e41b1f9d60c1412a93e
Colin Dess <pcolind@gmail.com>
2019-02-11 20:17:47 -0800

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express()
 app.use(function (req, res, next) {
 	res.header('Content-Type', 'application/json');
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	// res.setHeader('Access-Control-Allow-Methods', 'GET');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');	
+	res.setHeader('Access-Control-Allow-Methods', 'GET');
+	// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');	
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type')
 	next();
 })
