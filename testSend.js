251f19e4e18013c3e030d3f62c40b471e9355176
Colin Dess <pcolind@gmail.com>
2019-11-22 09:56:05 -0800

    Update testSend.js

--- testSend.js
+++ testSend.js
@@ -1,2 +1,15 @@
 var AWS = require("aws-sdk");
-console.log("Region: ", AWS.config.region);
+AWS.config.region = 'us-east-2b';
+// console.log("Region: ", AWS.config.region);
+
+var sns = AWS.SNS();
+
+var params = {
+	Message: 'test',
+	PhoneNumber: '+12064985138'
+};
+
+sns.publish(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
