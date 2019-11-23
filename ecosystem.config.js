76ce2f680a5363b4fa14f44d591bcf5b629bcf24
Colin Dess <pcolind@gmail.com>
2019-11-22 18:09:37 -0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
 			name: 'Wednesday',
 			script: './testSend.js',
 			instances: 1,
-    		cron_restart: "2 * * * *",
+    		cron_restart: "10 2 * * 6",
     		watch: false,
     		autorestart: false
 		}
