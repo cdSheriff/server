54bed45b7990fdfc3a0a517f9c48ad3541edcd18
Colin Dess <pcolind@gmail.com>
2019-11-30 10:36:48 -0800

    Update testSend.js

--- testSend.js
+++ testSend.js
@@ -7,7 +7,7 @@ var d = new Date();
 console.log(d.getHours());
 console.log(d.getDay());
 
-if (d.getHours()%2 != 0 || g.getDay() != 6) {
+if (d.getHours()%2 != 0 || d.getDay() != 6) {
 	console.log(`Current minutes is ${new Date().getMinutes()}, not running.`)
 	process.exit(0);
 }
